2d92db70b7b4bbbca27e2e216e4bdbb873decf62
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2014-01-27 19:10:11 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodeBeta::Application.config.secret_key_base = '6ae5763049ce55b29e94cabe55904e0ae9fdc769d1f28e6d4f894985f491654b2302446d7159cd19dd41287a3b1b0a3b7ebcb9e8a7f128bf7caa5890a5fa49cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodeBeta::Application.config.session_store :cookie_store, key: '_code_beta_session'
