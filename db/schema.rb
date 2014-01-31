2ef27b5a01873c531e4378affd05a785bedc7d79
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2014-01-30 20:35:17 -0500

    establsihed relationship between user and solutions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130021324) do
+ActiveRecord::Schema.define(version: 20140131013100) do
 
   create_table "problems", force: true do |t|
     t.string   "description"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20140130021324) do
     t.string   "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "solutions", ["user_id"], name: "index_solutions_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
