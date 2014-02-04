03550bfa687c2b03a767ab40329c32ecb9896926
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-02-04 09:19:18 -0500

    another fix to solutions controller

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -2,39 +2,47 @@ class SolutionsController < ApplicationController
   before_action :set_solution, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :current_problem
+  # before_action :current_problem
 
 
   def index
     @solutions = Solution.all
+    id = :problem_id
+    @problem =  Problem.find(params[id])
   end
 
 
   def show
     @solution = Solution.find(params[:id])
-    id = Solution.find(params[:problem_id])
-    @problem =  Problem.find_by(id: id )
+    # problem_id = Solution.find(params[:problem_id])
+    # problem_id = Solution.find(params[:problem_id])
+    @problem =  Problem.find_by(id: @solution.problem_id )
   end
 
 
   def new
     @solution = current_user.solutions.new
-    id = Solution.find(params[:problem_id])
-    @problem =  Problem.find_by(id: id )
+    # @problem =  Problem.find_by(id: @solution.problem_id )
+    id = :problem_id
+    @problem =  Problem.find(params[id]) 
   end
 
 
   def edit
     @solution = Solution.find(params[:id])
-    id = Solution.find(params[:problem_id])
+    # problem_id = Solution.find(params[:problem_id])
+    id = @solution.problem_id
     @problem =  Problem.find_by(id: id )
   end
 
 
   def create
-    problem = current_problem
+    id = :problem_id
+    @problem =  Problem.find(params[id]) 
+    # @problem =  Problem.find_by(id: @solution.problem_id )
+    # problem = current_problem
     @solution = current_user.solutions.build(solution_params)
-    @solution.problem_id = problem.id
+    @solution.problem_id = @problem.id
 
     if @solution.save
       redirect_to problem_solution_path(@problem, @solution), notice: 'Solution was successfully created.' 
@@ -45,7 +53,7 @@ class SolutionsController < ApplicationController
 
 
   def update
-    problem_id = Solution.find(params[:problem_id])
+    # problem_id = Solution.find(params[:problem_id])
 
     if @solution.update(solution_params)
       redirect_to problem_solution_path, notice: 'Solution was successfully updated.' 
@@ -57,6 +65,9 @@ class SolutionsController < ApplicationController
 
 
   def destroy
+    id = :problem_id
+    @problem =  Problem.find(params[id])
+    # @problem =  Problem.find_by(id: @solution.problem_id )
     @solution.destroy
     redirect_to problem_solutions_path(@problem) 
   end
@@ -65,6 +76,7 @@ class SolutionsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_solution
       @solution = Solution.find(params[:id])
+      # @problem =  Problem.find_by(id: @solution.problem_id )
     end
 
     def correct_user
@@ -72,11 +84,11 @@ class SolutionsController < ApplicationController
       redirect_to solutions_path, notice: "Not authorized to edit this solution" if @solution.nil?
     end
 
-    def current_problem
-      id = Solution.find(params[:problem_id])
-      @problem =  Problem.find_by(id: id ) 
+    # def current_problem
+    #   problem_id = Solution.find(params[:problem_id])
+    #   @problem =  Problem.find_by(id: problem_id ) 
 
-    end
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def solution_params
