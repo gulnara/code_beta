57115372c494c59a6fa473eb5a483596d8b338e6
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-02-06 07:37:39 -0500

    users now can create edit and deelete their problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
-
+  has_many :created_problems, class_name: 'Problem'
 
 end
