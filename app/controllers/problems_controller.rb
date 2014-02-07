dcd46ed2bb4e118c6e0681dc6680ec67421385d1
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-02-06 20:26:23 -0500

    admin now can delete both solutions and problems of any user

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,58 +1,27 @@
 class ProblemsController < ApplicationController
   before_action :set_problem, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :are_you_admin_or_creater, only: [:edit, :update, :destroy]
 
 
 
-  # GET /problems
-  # GET /problems.json
   def index
     @problems = Problem.all
   end
 
-  # GET /problems/1
-  # GET /problems/1.json
   def show
-
   end
-
-  # GET /problems/new
-  # def new
-  #   if current_user.try(:admin?) 
-  #     @problem = Problem.new
-  #   else
-  #     redirect_to problems_path, notice: "Not authorized to create a problem"
-  #   end
-  # end
 
   def new
     @problem = current_user.problems.new
-    # @problem =  Problem.find_by(id: @solution.problem_id )
-    # id = :problem_id
-    # @problem =  Problem.find(params[id]) 
+
   end
 
-  # GET /problems/1/edit
+
   def edit
-
   end
 
-  # POST /problems
-  # POST /problems.json
-  # def create
-  #   if current_user.try(:admin?) 
-  #     @problem = Problem.new(problem_params)
-  #     if @problem.save
-  #       redirect_to @problem, notice: 'Problem was successfully created.' 
-  #     else
-  #       render action: 'new' 
-  #     end
-  #   else
-  #     redirect_to problems_path, notice: "Not authorized to create a problem"
-  #   end
-  # end
+
 
   def create
     @problem = current_user.problems.build(problem_params)
@@ -65,8 +34,7 @@ class ProblemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /problems/1
-  # PATCH/PUT /problems/1.json
+
   def update
 
     if @problem.update(problem_params)
@@ -77,8 +45,7 @@ class ProblemsController < ApplicationController
 
   end
 
-  # DELETE /problems/1
-  # DELETE /problems/1.json
+
   def destroy
     @problem.destroy
     redirect_to problems_url 
@@ -86,7 +53,7 @@ class ProblemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
     def set_problem
       @problem = Problem.find(params[:id])
     end
@@ -99,7 +66,7 @@ class ProblemsController < ApplicationController
       end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+ 
     def problem_params
       params.require(:problem).permit(:description, :title, :source, :user_id)
     end
--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,8 +1,8 @@
 class SolutionsController < ApplicationController
   before_action :set_solution, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :are_you_admin_or_creater, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :current_problem
+
 
 
   def index
@@ -14,14 +14,12 @@ class SolutionsController < ApplicationController
 
   def show
     @solution = Solution.find(params[:id])
-    # problem_id = Solution.find(params[:problem_id])
     @problem =  Problem.find_by(id: @solution.problem_id )
   end
 
 
   def new
     @solution = current_user.solutions.new
-    # @problem =  Problem.find_by(id: @solution.problem_id )
     id = :problem_id
     @problem =  Problem.find(params[id]) 
   end
@@ -29,7 +27,6 @@ class SolutionsController < ApplicationController
 
   def edit
     @solution = Solution.find(params[:id])
-    # problem_id = Solution.find(params[:problem_id])
     id = @solution.problem_id
     @problem =  Problem.find_by(id: id )
   end
@@ -38,8 +35,6 @@ class SolutionsController < ApplicationController
   def create
     id = :problem_id
     @problem =  Problem.find(params[id]) 
-    # @problem =  Problem.find_by(id: @solution.problem_id )
-    # problem = current_problem
     @solution = current_user.solutions.build(solution_params)
     @solution.problem_id = @problem.id
 
@@ -52,8 +47,6 @@ class SolutionsController < ApplicationController
 
 
   def update
-    # problem_id = Solution.find(params[:problem_id])
-
     if @solution.update(solution_params)
       redirect_to problem_solution_path, notice: 'Solution was successfully updated.' 
     else
@@ -66,30 +59,26 @@ class SolutionsController < ApplicationController
   def destroy
     id = :problem_id
     @problem =  Problem.find(params[id])
-    # @problem =  Problem.find_by(id: @solution.problem_id )
     @solution.destroy
     redirect_to problem_solutions_path(@problem) 
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_solution
       @solution = Solution.find(params[:id])
-      # @problem =  Problem.find_by(id: @solution.problem_id )
     end
 
-    def correct_user
-      @solution = current_user.solutions.find_by(id: params[:id])
-      redirect_to solutions_path, notice: "Not authorized to edit this solution" if @solution.nil?
+    def are_you_admin_or_creater
+      if current_user.try(:admin?) or @solution.user == current_user
+        @solution = Solution.find(params[:id])
+        # @solution = current_user.solutions.find_by(id: params[:id])
+      else  
+        redirect_to solutions_path, notice: "Not authorized to edit this solution" if @solution.nil?
+      end
     end
 
-    # def current_problem
-    #   problem_id = Solution.find(params[:problem_id])
-    #   @problem =  Problem.find_by(id: problem_id ) 
-
-    # end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
+   
     def solution_params
       params.require(:solution).permit(:answer, :problem_id, :id, :language )
     end
