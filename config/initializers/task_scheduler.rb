fa5c8e95f6a0ad96267b16f9baf083b9e54c1464
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-02-15 08:06:45 -0500

    updated scheduling to each day

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -8,7 +8,7 @@ require 'rufus/scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every('1w') do
+scheduler.every('1d') do
 	puts "Emailing problems"
 	UsersController.send_problems_to_users
 end
