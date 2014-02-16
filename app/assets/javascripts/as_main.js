9b0ef7a1b25454b7e5c1a157f1cfc5c26867ee70
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-02-16 11:46:52 -0500

    updated css

--- app/assets/javascripts/as_main.js
+++ app/assets/javascripts/as_main.js
@@ -3,20 +3,21 @@ jQuery(document).ready(function($) {
 
   // bind to the left button
   $('.left-button').click(function(event) {
-    // toggle the visibility of the left sidebar
     $('.left-sidebar').toggle();
-    // toggle the class 'left-active' on the .content div
     $('.content').toggleClass('left-active');
+    $('.small').toggleClass('left-active');
+    $('.right-button').toggleClass('left-active');
+
   });
 
   $('.right-button').click(function(event) {
     $('.right-sidebar').toggle();
     $('.content').toggleClass('right-active');
+    $('.small').toggleClass('right-active');
+    $('.left-button').toggleClass('right-active');
   });
 
   $('.toggle-footer-menu').click(function(event) {
-    // toggle the visibility of the closed list in the footer menu and slide it
-    // into place
     $('.footer-menu .closed').slideToggle();
     $('.footer-menu .open').slideToggle();
   });
